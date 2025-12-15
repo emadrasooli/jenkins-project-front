eea03a1a736dda8829f624ded65bdc1e81ae69cf
M.EmadRasooli <rasooliemad2004@gmail.com>
2025-12-15 14:46:38 +0430

    test jenkins 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
       <h1 className="text-[300px] text-white font-bold">Welcome!</h1>
       <p className="w-3xl text-center text-zinc-600 text-lg text-pretty">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
+        industry.
       </p>
     </div>
   );
