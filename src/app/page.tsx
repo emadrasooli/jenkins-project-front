b421891ceb472abe9fffa2dc58915b369f5fbf85
M.EmadRasooli <rasooliemad2004@gmail.com>
2025-12-15 13:43:00 +0430

    creat some changes on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="bg-blue-950 min-h-screen flex items-center justify-center">
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-[300px] text-white font-bold">Welcome!</h1>
+      <p className="w-3xl text-center text-zinc-600 text-lg text-pretty">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
     </div>
   );
 }
